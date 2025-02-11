3e6e496b0e89a5ed96d0e103148d30d2822bc827
Sli <antoine@bartuccio.fr>
2025-02-12 00:11:45 +0100

    Basic layout for main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,54 @@
 <script lang="ts">
-	import Calendar from "../components/Calendar.svelte"
+  import Calendar from "../components/Calendar.svelte"
 </script>
 
-<h1>NTX Events</h1>
 
-<h2>Instagram</h2>
+<div class="container">
 
-<h3>Événements</h3>
+  <div class="row">
+    <h1>NTX Events</h1>
 
-<Calendar />
+    <div class="col-md-9">
+      <section>
+        <h2>Instagram</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </section>
+
+      <section>
+        <h2>Événements</h2>
+        
+      </section>
+
+      <Calendar />
+    </div>
+
+    <div class="col-md">
+      <section>
+        <h2>Réseaux sociaux</h2>
+        <ul>
+          <li>
+            <a
+              href="https://www.instagram.com/ntxevent/"
+              rel="nofollow"
+              target="_blank"
+            >Instagram</a>
+          </li>
+          <li>
+            <a
+              href="https://www.facebook.com/profile.php?id=61559830509940"
+              rel="nofollow"
+              target="_blank"
+              >Facebook</a>
+          </li>
+        </ul>
+      </section>
+    </div>
+
+  </div>
+
+</div>
